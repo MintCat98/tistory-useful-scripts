77a2b317ea674aef8e8728f19804db81d73723b1
Mincheol <presidentmc9898@gmail.com>
2025-08-19 16:06:48 +0900

    Update darkmode_color_fix.js

--- darkmode_color_fix.js
+++ darkmode_color_fix.js
@@ -1,20 +1,24 @@
-// 출처: https://dev-mintcat.tistory.com/12 [Dev-MintCat:티스토리]
 <script>
 document.addEventListener("DOMContentLoaded", function () {
     const paragraphs = document.querySelectorAll("p");
 
     paragraphs.forEach(p => {
-        // 1. data-ke-size는 보존
-        const keSize = p.getAttribute("data-ke-size");
+        if (p.style.color) {
+            p.style.color = '';
+        }
 
-        // 2. 기존 속성 모두 제거
-        [...p.attributes].forEach(attr => {
-            p.removeAttribute(attr.name);
+        const attributesToKeep = ['data-ke-size', 'style'];
+      
+        const currentAttributes = [...p.attributes];
+
+        currentAttributes.forEach(attr => {
+            if (!attributesToKeep.includes(attr.name)) {
+                p.removeAttribute(attr.name);
+            }
         });
-
-        // 3. data-ke-size만 다시 추가
-        if (keSize) {
-            p.setAttribute("data-ke-size", keSize);
+        
+        if (p.getAttribute('style') === '') {
+            p.removeAttribute('style');
         }
     });
 });
